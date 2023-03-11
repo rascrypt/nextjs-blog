401559ca9fe336e1800a35ac01b950e6c98c659a
rascrypt <75642818+vinceeverton@users.noreply.github.com>
2023-03-10 20:54:15 -0500

    Update layout.js

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'rascrypt';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'rascrypt blog';
 
 export default function Layout({ children, home }) {
   return (
